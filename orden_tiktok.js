6a206a99190585451897732c1c7c646cb07b58d8
Pichulita321 <pichulita321@users.noreply.github.com>
2026-08-12 02:29:56 -0500

    Use 100% exact text line parser from Castigo3D to prevent JS syntax error crashes

--- orden_tiktok.js
+++ orden_tiktok.js
@@ -2,5 +2,3 @@
 // ?? ARCHIVO DE ACCIÓN DE STREAMER.BOT (ALERTAS TIKTOK LIVE)
 // ==========================================
 
-lanzarAlertaTikTok(" Donador_Prueba_30\, 30, \\, Date.now(), \Troleo_Raton_Borracho\);
-lanzarAlertaTikTok(" Seguidor_Prueba_Directa\, 0, \\, Date.now(), \follow\);
